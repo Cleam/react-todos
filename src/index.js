a58935e76615ce50e18a66e78447034b9e5e8997
lizhigaomac <lizhigao@021.com>
2018-11-30 15:09:36 +0800

    redux with router

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { render } from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
-import todoApp from './reducers'
-import App from './components/App'
-import logger from "redux-logger";
+import todoApp from './reducers';
+import Root from './components/Root';
+
+import logger from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  todoApp,
-  composeEnhancers(
-    applyMiddleware(logger)
-  )
-);
+const store = createStore(todoApp, composeEnhancers(applyMiddleware(logger)));
 
 // let store = createStore(todoApp)
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+render(<Root store={store} />, document.getElementById('root'));
 
 registerServiceWorker();
